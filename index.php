b4c475b19fc83bcbc6048da8ee7ae438fa9b76f3
Ivonne Mtz <Ivonnemtz96@outlook.es>
2023-03-27 09:47:18 -0700

    Se eliminan archivos de plantilla anterior

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<?php
+    include("includes/head.php");
+?>
+<body>
+    <?php
+        include("includes/whats.php");
+        include("modules/index.php");
+        include("includes/whats.php");
+        include("includes/footer.php");
+        include("includes/scripts.php");
+    ?>
+
+</body>
+
+</html>
