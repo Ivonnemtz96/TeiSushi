6ab5d02ee8d2752fe650639104c469b8b77a8044
Ivonne Mtz <Ivonnemtz96@outlook.es>
2023-03-29 15:18:42 -0700

    menú ok

---
+++ menu.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<?php
+    $title = "Inicio";
+    include("includes/head.php");
+?>
+<body>
+    <?php
+        include("includes/header.php");
+        include("includes/preloader.php");
+        include("modules/menu.php");
+        include("includes/whats.php");
+        include("includes/footer.php");
+        include("includes/scripts.php");
+    ?>
+
+</body>
+
+</html>
